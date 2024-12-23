ed2bff5a03b2d4d8d0727c661c74e4c483114b87
Hamzeh <h.qorbani7@gmail.com>
2024-12-23 23:37:56 +0330

    session_0: introduce

---
+++ lessons/session_0/use_list.py
@@ -0,0 +1,19 @@
+name = "Hamzeh"
+# variable = value
+# print(name)
+# names = ["Hamzeh" , "Omid" , "Mina" , 65 , 152.12]
+names = ["Hamzeh" , "Omid" , "Mina" , "Omid "]
+print(names[0])
+print(names[1])
+print(names[2])
+print()
+for item in names:
+    # indent
+    if item == "Omid":
+        print("Omid student")
+    else:
+        print(item)
+print("------")
+
+
+
